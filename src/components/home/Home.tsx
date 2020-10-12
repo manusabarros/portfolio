a3540c52459710c2ce86a0dfb0dce25aecae1221
Manu Sabarrós <manusabarros@gmail.com>
2020-10-12 16:41:49 -0300

    fix background image

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,21 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./Home.scss";
-import background from "../../assets/vinyl3.jpg";
 import Name from "../name/Name";
 
 const Home = () => {
-    // eslint-disable-next-line no-restricted-globals
-    const [width, setWidth] = useState(undefined as any); const [height, setHeight] = useState(undefined as any);
     const [name, setName] = useState(true);
 
-    window.addEventListener("resize", () => {
-        if (width <= window.innerWidth) setWidth(window.innerWidth);
-    });
-
     useEffect(() => {
-        // eslint-disable-next-line no-restricted-globals
-        if (screen.width > screen.height) setWidth(screen.width);
-        else setHeight(window.innerHeight);
         setInterval(() => {
             setName(false);
             setName(true);
@@ -24,9 +14,7 @@ const Home = () => {
 
     return (
         <div className="Home">
-            <div className="background">
-                <img src={background} alt="building" width={width} height={height} />
-            </div>
+            <div className="background"></div>
             {name && <Name />}
         </div>
     );
