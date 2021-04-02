f4f22389a6950effdab1a677413117bb4f195df4
Manuel Sabarros <manusabarros@Manuels-MacBook-Pro.local>
2021-04-02 01:52:04 -0300

    fix technologies

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -7,7 +7,7 @@ import { useTranslation } from "react-i18next";
 
 const Skills = forwardRef((props: any, ref: any) => {
     const { t } = useTranslation();
-    const frontendSkills = ["HTML - CSS - SASS", "JavaScript - TypeScript", "React / Angular", "React Native / Ionic", "Next.js"];
+    const frontendSkills = ["HTML - CSS - SASS", "JavaScript - TypeScript", "React - Angular", "React Native - Ionic", "Next.js"];
     const backendSkills = ["Node.js", "REST APIs (Express.js)", "GraphQL (Apollo)", "SQL (PostgreSQL)", "NoSQL (MongoDB)"];
     const othersSkills = ["GIT", "GNU/Linux", "Docker", "AWS (EC2, S3, RDS, Route53)", t("AGILE_METHODOLOGIES")];
 
